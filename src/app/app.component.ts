b2bed9621d0cc8ab34b7dad22df682b4808ba1d4
Miguel Luiz de Albuquerque <miguelluizdealbuquerque@gmail.com>
2023-05-21 22:27:49 -0300

    Bindings
    propertie bindings
    Event binding button
    Two way data bidings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,24 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterViewInit ,AfterContentChecked, AfterViewChecked{
+export class AppComponent  {
   title = 'Bem vindo ao treinamento de Angular';
+
+  imgUrl= "https://res.cloudinary.com/practicaldev/image/fetch/s--NfSR39p4--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/tc7pnlyowg52jw2px5at.png";
 
   constructor(){
     console.log("Component constructor")
    }
 
-   ngAfterViewChecked(): void {
-    console.log("View Checked")
+   chamarFuncao(){
+    console.log("esse é um click")
    }
-    ngAfterContentChecked(): void {
-    console.log("Component Content Checked")
-    }
-  ngAfterViewInit(): void {
-    console.log("Component AfterViewInit")
-  }
-  ngAfterContentInit(): void {
-    console.log("Component AfterContentInit")
-  }
-  ngOnDestroy(): void {
-    console.log("Component OnDestroy")
-  }
-ngOnChanges(changes: SimpleChanges): void {
-  console.log("Component onChanges")
-}
 
-  ngOnInit(): void {
-    console.log("Component onInit")
-    
-  }
-
-ngDoCheck(): void {
-  console.log("Component doCheck")
-}
-mudarTexto(){
-  this.title += 'a';
-}
-
+  
+  
 }
