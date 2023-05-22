ad7a59e8c03a077a384c33258e4138c3abd53ab7
Miguel Luiz de Albuquerque <miguelluizdealbuquerque@gmail.com>
2023-05-21 22:00:12 -0300

    first commit
    aula 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'projeto-1';
+export class AppComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterViewInit ,AfterContentChecked, AfterViewChecked{
+  title = 'Bem vindo ao treinamento de Angular';
+
+  constructor(){
+    console.log("Component constructor")
+   }
+
+   ngAfterViewChecked(): void {
+    console.log("View Checked")
+   }
+    ngAfterContentChecked(): void {
+    console.log("Component Content Checked")
+    }
+  ngAfterViewInit(): void {
+    console.log("Component AfterViewInit")
+  }
+  ngAfterContentInit(): void {
+    console.log("Component AfterContentInit")
+  }
+  ngOnDestroy(): void {
+    console.log("Component OnDestroy")
+  }
+ngOnChanges(changes: SimpleChanges): void {
+  console.log("Component onChanges")
+}
+
+  ngOnInit(): void {
+    console.log("Component onInit")
+    
+  }
+
+ngDoCheck(): void {
+  console.log("Component doCheck")
+}
+mudarTexto(){
+  this.title += 'a';
+}
+
 }
