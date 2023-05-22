2a85b62f1772b10fa2c126de571b13b4e1941f2e
Miguel Luiz de Albuquerque <miguelluizdealbuquerque@gmail.com>
2023-05-21 22:51:34 -0300

    diretivas de comando ngIf
    diretivas de comando pt1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,14 @@ import { Component } from "@angular/core";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent  {
-  title = 'Bem vindo ao treinamento de Angular';
 
-  imgUrl= "https://res.cloudinary.com/practicaldev/image/fetch/s--NfSR39p4--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/tc7pnlyowg52jw2px5at.png";
-
+  title ="";
+  
   constructor(){
     console.log("Component constructor")
    }
 
-   chamarFuncao(){
-    console.log("esse é um click")
-   }
+ 
 
   
   
